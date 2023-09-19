751e001884fb0828889adba6e525c663fad0216e
Angel Eduardo Lopez Solis <a00837012@tec.mx>
2023-09-19 01:56:11 -0600

    Added todo list

--- edgyboy.ino
+++ edgyboy.ino
@@ -3,6 +3,25 @@
 static const uint8_t leftIRPin = 10;
 static const uint8_t rightIRPin = 10;
 
+/**
+ * Todo list:
+ * Add Motor lib
+ * Add Encoder lib
+ * Add PID lib
+ * Add servo logic
+ * Add color sensor lib
+ * Add color sensor logic
+ * Add IR sensor lib
+ * Add IR sensor logic
+ * Auto-zone Detect zones automatically
+ * Program Zone A
+ * Program Zone B
+ * Program Zone C
+ * Program slope zone
+ * Program PID control for movement
+ * Program PID control for pivot
+ */
+
 void setup()
 {
     
